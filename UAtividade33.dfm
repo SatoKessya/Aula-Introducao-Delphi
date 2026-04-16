99e210441e2ef86536355ae8110a941e118ad7c2
SatoKessya <Kessya.sato@icloud.com>
2026-04-15 22:38:04 -0300

    Aula 15-04

--- UAtividade33.dfm
+++ UAtividade33.dfm
@@ -2,8 +2,8 @@ object frmAtividade33: TfrmAtividade33
   Left = 0
   Top = 0
   Caption = 'Atividade 33'
-  ClientHeight = 441
-  ClientWidth = 624
+  ClientHeight = 308
+  ClientWidth = 442
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,30 +11,51 @@ object frmAtividade33: TfrmAtividade33
   Font.Name = 'Segoe UI'
   Font.Style = []
   TextHeight = 45
+  object lblNome: TLabel
+    Left = 19
+    Top = 24
+    Width = 95
+    Height = 45
+    Caption = 'Nome:'
+  end
+  object lblValorHora: TLabel
+    Left = 19
+    Top = 95
+    Width = 102
+    Height = 45
+    Caption = '$ Hora:'
+  end
+  object lblHorasMes: TLabel
+    Left = 19
+    Top = 167
+    Width = 179
+    Height = 45
+    Caption = 'Horas (M'#234's):'
+  end
   object edtNome: TEdit
-    Left = 168
-    Top = 32
-    Width = 289
+    Left = 216
+    Top = 20
+    Width = 209
     Height = 53
     TabOrder = 0
   end
   object edtHora: TEdit
-    Left = 168
-    Top = 109
-    Width = 289
+    Left = 216
+    Top = 91
+    Width = 209
     Height = 53
     TabOrder = 1
   end
   object edtHorasMes: TEdit
-    Left = 168
-    Top = 184
-    Width = 289
+    Left = 216
+    Top = 163
+    Width = 209
     Height = 53
     TabOrder = 2
   end
   object btnCalcular: TButton
-    Left = 232
-    Top = 264
+    Left = 129
+    Top = 232
     Width = 171
     Height = 53
     Caption = 'Calcular'
--- UAtividade33.pas
+++ UAtividade33.pas
@@ -12,6 +12,9 @@ type
     edtHora: TEdit;
     edtHorasMes: TEdit;
     btnCalcular: TButton;
+    lblNome: TLabel;
+    lblValorHora: TLabel;
+    lblHorasMes: TLabel;
     procedure btnCalcularClick(Sender: TObject);
   private
     { Private declarations }
@@ -28,11 +31,64 @@ implementation
 
 procedure TfrmAtividade33.btnCalcularClick(Sender: TObject);
 var
-  salarioBruto: real;
+  salarioHora: Real;
+  qtdeHoras: Integer;
+  salarioBruto: Real;
+  descontoIR: Real;
+  descontoINSS: Real;
+  descontoSindicato: Real;
+  descontoFGTS: Real;
+  totalDescontos: Real;
+  salarioLiquido: Real;
 
 begin
-  ('Salário:' salarioBruto);
+  salarioHora := StrToFloat(EdtHora.Text);
+  qtdeHoras := StrToInt(EdtHorasMes.Text);
 
+  // SALÁRIO BRUTO
+  salarioBruto := salarioHora * qtdeHoras;
+
+  // IMPOSTO DE RENDA
+  if salarioBruto <= 1900 then
+    descontoIR := 0
+
+  else if salarioBruto <= 3500 then
+    descontoIR := salarioBruto * 0.05
+
+  else if salarioBruto <= 7500 then
+    descontoIR := salarioBruto * 0.10
+
+  else
+  descontoIR := salarioBruto * 0.20;
+
+  // INSS (10%)
+  descontoINSS := salarioBruto * 0.10;
+
+  // SINDICATO (3%)
+  descontoSindicato := salarioBruto * 0.03;
+
+  // FGTS (11%)
+  descontoFGTS := salarioBruto * 0.11;
+
+  // TOTAL DESCONTOS
+  totalDescontos := descontoIR + descontoINSS + descontoSindicato;
+
+  // SALÁRIO LÍQUIDO
+  salarioLiquido := salarioBruto - totalDescontos;
+
+  MessageBox(Application.Handle,
+  PChar (
+    ' Nome: ' + edtNome.Text + #13#10 +
+    ' | Salário Bruto: ' + FloatToStr(salarioBruto) + #13#10 +
+    ' | (-) IR: ' + FloatToStr(descontoIR) + #13#10 +
+    ' | (-) INSS: ' + FloatToStr(descontoINSS) + #13#10 +
+    ' | (-) Sindicato: ' + FloatToStr(descontoSindicato) + #13#10 +
+    ' | FGTS: ' + FloatToStr(descontoFGTS) + #13#10 +
+    ' | Total de descontos: ' + FloatToStr(totalDescontos) + #13#10 +
+    ' | Salário Líquido: ' + FloatToStr(salarioLiquido)
+   ),
+   'Holerite',
+   0);
 end;
 
 end.
